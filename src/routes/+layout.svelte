038d3e56324c8003913f4b6dd2e3d27ec7899235
aaronrubinstein <40306940+aaronrubinstein@users.noreply.github.com>
2025-12-22 06:43:55 +0000

    Initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,85 @@
+<script lang="ts">
+	import favicon from '$lib/assets/favicon.svg';
+	import { browser } from '$app/environment';
+	import '$lib/app.css';
+	import Sun from '$lib/components/icons/Sun.svelte';
+	import Moon from '$lib/components/icons/Moon.svelte';
+
+	let { children } = $props();
+
+	let darkMode = $state((browser && localStorage.getItem('theme') === 'dark') || false);
+
+	const toggleTheme = () => {
+		let newTheme = darkMode ? 'light' : 'dark';
+		localStorage.setItem('theme', newTheme);
+		document.body.setAttribute('data-theme', newTheme);
+		darkMode = newTheme === 'dark';
+	};
+
+	const test = () => {
+		console.log('test');
+	};
+</script>
+
+<svelte:head>
+	<title>Character Counter</title>
+	<link rel="icon" href={favicon} />
+	<link rel="preconnect" href="https://fonts.googleapis.com" />
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="anonymous" />
+	<link
+		href="https://fonts.googleapis.com/css2?family=DM+Sans:ital,opsz,wght@0,9..40,100..1000;1,9..40,100..1000&display=swap"
+		rel="stylesheet"
+	/>
+</svelte:head>
+
+<div class="content">
+	<header>
+		{#if darkMode}
+			<img src="/logo-dark-theme.svg" alt="Character Counter Logo" />
+			<button type="button" onclick={toggleTheme}>
+				<Sun />
+			</button>
+		{:else}
+			<img src="/logo-light-theme.svg" alt="Character Counter Logo" />
+			<button type="button" onclick={toggleTheme}>
+				<Moon />
+			</button>
+		{/if}
+	</header>
+
+	{@render children()}
+</div>
+
+<style>
+	.content {
+		max-width: 990px;
+		margin: 34px auto;
+	}
+
+	header {
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+	}
+
+	button {
+		width: 44px;
+		height: 44px;
+		display: grid;
+		place-items: center;
+		border-radius: 8px;
+		background: var(--input-bg);
+	}
+
+	@media (width < 1070px) {
+		.content {
+			margin: 16px 40px;
+		}
+	}
+
+	@media (width < 768px) {
+		.content {
+			margin: 16px 16px;
+		}
+	}
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,25 @@
+<h1>
+	Analyze your text<br />
+	in real-time
+</h1>
+
+<style>
+	h1 {
+		font-size: 64px;
+		font-weight: 700;
+		line-height: 100%;
+		letter-spacing: -1px;
+		color: var(--title-text);
+		margin: 48px 0;
+		text-align: center;
+	}
+
+	@media (width < 768px) {
+		h1 {
+			font-size: 40px;
+		}
+		br {
+			display: none;
+		}
+	}
+</style>
