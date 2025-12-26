5f91a308ff600a279ab363c21d58982e729ee93b
aaronrubinstein <40306940+aaronrubinstein@users.noreply.github.com>
2025-12-26 06:22:23 +0000

    Replace adapter-auto with adapter-static in configuration files; add prerender setting

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -6,9 +6,9 @@ const config = {
 	// Consult https://svelte.dev/docs/kit/integrations
 	// for more information about preprocessors
 	preprocess: vitePreprocess(),
-	compilerOptions: {
-		runes: true
-	},
+
+	compilerOptions: { runes: true },
+
 	// Set runes mode to undefined for node_modules
 	// https://github.com/ciscoheat/sveltekit-superforms/issues/306
 	vitePlugin: {
@@ -18,12 +18,8 @@ const config = {
 			}
 		}
 	},
-	kit: {
-		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter()
-	}
+
+	kit: { adapter: adapter() }
 };
 
 export default config;
